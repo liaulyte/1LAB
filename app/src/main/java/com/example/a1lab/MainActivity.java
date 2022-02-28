eacc599ec90b73af1df468383eab88d677676927
LauraSub <laura.subaciut@gmail.com>
2022-02-28 21:34:51 +0200

    push with new text

--- app/src/main/java/com/example/a1lab/MainActivity.java
+++ app/src/main/java/com/example/a1lab/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.a1lab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvmain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvmain=findViewById(R.id.tvmain);
     }
 
+    public void OnBtnclick(View view) {
+        this.tvmain.setText("Text was changed!");
+    }
 }
