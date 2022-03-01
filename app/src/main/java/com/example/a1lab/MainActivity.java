b54138b9607bb15b9a15bc2189fe5aa14aa0b3f0
LauraSub <laura.subaciut@gmail.com>
2022-03-01 17:35:17 +0200

    added button for color change in text

--- app/src/main/java/com/example/a1lab/MainActivity.java
+++ app/src/main/java/com/example/a1lab/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.a1lab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -9,6 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvmain;
+    TextView tvmainColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.tvmain=findViewById(R.id.tvmain);
+        this.tvmainColor=findViewById(R.id.txtChangeColor);
+
     }
 
     public void OnBtnclick(View view) {
-        this.tvmain.setText("Text was changed!");
+        this.tvmain.setText(R.string.changed_label_caption);
+    }
+    public void OnBtnclickChangeColor(View view) { this.tvmainColor.setTextColor(Color.GREEN);
     }
 }
